7b7716ea6f7e31ffca4ed986d429c0911c825ca3
abdrizik <abdelrahman.rizik@gmail.com>
2026-09-14 12:52:54 +0300

    feat(separator): add separator

---
+++ packages/shardsui/src/components/separator/separator.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed, mergeProps } from 'vue'
+import { dataAttrs } from '@/internal/data-attrs'
+import type { Orientation, PartProps } from '@/internal/types'
+
+type Props = PartProps & {
+  orientation?: Orientation
+}
+
+defineOptions({ inheritAttrs: false })
+
+const { as = 'div', orientation = 'horizontal' } = defineProps<Props>()
+
+const stateAttrs = computed(() => dataAttrs({ orientation }))
+
+const ownAttrs = computed(() => ({ role: 'separator', 'aria-orientation': orientation }))
+</script>
+
+<template>
+  <component :is="as" v-bind="mergeProps(stateAttrs, ownAttrs, $attrs)" />
+</template>
