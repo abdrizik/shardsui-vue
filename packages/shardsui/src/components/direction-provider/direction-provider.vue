dad47e5c6d63e29050695cec33127b9fd695f792
abdrizik <abdelrahman.rizik@gmail.com>
2026-09-14 12:52:54 +0300

    feat(direction-provider): add direction-provider

---
+++ packages/shardsui/src/components/direction-provider/direction-provider.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { DirectionContext, type TextDirection } from '@/internal/direction-context'
+
+type Props = {
+  direction?: TextDirection
+}
+
+defineOptions({ inheritAttrs: false })
+
+const { direction = 'ltr' } = defineProps<Props>()
+
+defineSlots<{ default?: () => any }>()
+
+DirectionContext.set({ direction: computed(() => direction) })
+</script>
+
+<template>
+  <slot />
+</template>
---
+++ packages/shardsui/src/components/direction-provider/index.ts
@@ -0,0 +1,3 @@
+export { getDirection } from '@/internal/direction-context'
+export type { TextDirection } from '@/internal/direction-context'
+export { default as DirectionProvider } from './direction-provider.vue'
